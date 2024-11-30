2966fb230ee46c117925d6a622a8a2f2daa3e8da
Grzegorz Grzęda <grzegorz.grzeda@g2labs.pl>
2024-11-30 16:54:43 +0100

    Add license and formatting

--- embedded_c/connection/connection.c
+++ embedded_c/connection/connection.c
@@ -21,27 +21,4 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-#ifndef CONNECTION_H
-#define CONNECTION_H
-
-typedef struct connection connection_t;
-
-typedef void (*connection_receive_callback_t)(connection_t* connection,
-                                              void* data,
-                                              int size);
-
-connection_t* connection_create(const char* host,
-                                int port,
-                                connection_receive_callback_t callback);
-
-void connection_destroy(connection_t* connection);
-
-void connection_open(connection_t* connection);
-
-void connection_close(connection_t* connection);
-
-void connection_send(connection_t* connection, void* data, int size);
-
-void connection_receive(connection_t* connection, void* data, int size);
-
-#endif  // CONNECTION_H
+#include "connection.h"
